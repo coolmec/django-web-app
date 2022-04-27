e9f84467cf03955189507f21343e2bd2aa1fbe61
coolmec <jmoukaila@yahoo.fr>
2022-04-27 23:21:46 +0000

    updates

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from listings.models import Band
+from listings.models import Band, Announce
 
 # Create your views here.
 def hello(request):
+    context = {}
     bands = Band.objects.all()
+    context['bands'] = bands
+    return render(request, 'hello.html', context)
+
+
+def listings(request):
+    bands = Band.objects.all()
+    announces = Announce.objects.all()
     return HttpResponse(f"""
                         <h1>Hello World !</h1>
                         <p>Mes groupes préférés sont :</p>
@@ -13,11 +21,13 @@ def hello(request):
                             <li>{bands[1].name}</li>
                             <li>{bands[2].name}</li>
                         </ul>
+                        <p>Les annonces disponibles:</p>
+                        <ul>
+                            <li>{announces[0].title}</li>
+                            <li>{announces[1].title}</li>
+                            <li>{announces[2].title}</li>
+                        </ul>
                         """)
-
-
-def listings(request):
-    return HttpResponse("<h1>Hello World !</h1>")
 
 
 def contact_us(request):
